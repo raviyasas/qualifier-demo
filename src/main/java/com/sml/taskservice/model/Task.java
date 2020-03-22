edc6233b7bcf38f6fa7af1e35048add65905e899
raviyasas <raviyasas@live.com>
2020-03-22 22:31:41 +0530

    Initial commit

---
+++ src/main/java/com/sml/taskservice/model/Task.java
@@ -0,0 +1,33 @@
+package com.sml.taskservice.model;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import lombok.Data;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+import java.io.Serializable;
+import java.util.Date;
+import java.util.List;
+
+@Entity
+@Data
+@Table(name = "tasks")
+public class Task implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue
+    @Column(name = "task_id", nullable = false, unique = true)
+    private Integer id;
+
+    @Column(name = "task_name", nullable = false)
+    @NotEmpty(message = "Task name should not be empty")
+    private String taskName;
+
+    @Temporal(TemporalType.DATE)
+    private Date taskStartedDate;
+
+
+
+}
